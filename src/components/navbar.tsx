3b03be47d2066b78e245be782c2c250e9a87364e
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-09-24 14:54:49 -0700

    add logo & favicon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,16 @@
 'use client';
 import { useUser, SignInButton, SignOutButton } from '@clerk/nextjs';
+import Image from 'next/image';
 export default function Navbar() {
   const { user, isSignedIn } = useUser();
   return (
     <div className="bg-black-pearl-900 flex h-[60px] items-center justify-between">
-      <div className="mx-5 flex space-x-5">LOGO HERE</div>
+      <div className="mx-5 flex space-x-5">
+        <div className="flex items-center gap-2">
+          <Image src="/journal.svg" width={30} height={60} alt="Logo" />
+          <span>Journal</span>
+        </div>
+      </div>
       <div className="mx-5 flex space-x-5">
         {!isSignedIn && <SignInButton />}
         {isSignedIn && (
