593029686990003d8efd30d7a8008f569b942e4d
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-10-24 15:53:19 -0700

    add sliding navbar for mobile devices

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -23,69 +23,75 @@ export default function Navbar({ showMenu }: { showMenu?: boolean }) {
   const displayName =
     (user?.publicMetadata?.displayName as string) ?? user?.firstName;
   return (
-    <div className="flex h-[var(--navbar-height)] items-center justify-between bg-white text-gray-600">
-      <div>
-        <div
-          className={cn('mx-5 hidden space-x-5 lg:flex', !showMenu && '!flex')}
-        >
-          <Link
-            className={cn('flex items-center gap-3', logoFont.className)}
-            href="/"
+    <>
+      <div className="fixed flex h-[var(--navbar-height)] w-full items-center justify-between bg-white text-gray-600 lg:relative">
+        <div>
+          <div
+            className={cn(
+              'mx-5 hidden space-x-5 lg:flex',
+              !showMenu && '!flex'
+            )}
           >
-            <Image src="/journal.svg" width={25} height={60} alt="Logo" />
-            <span className="hidden pt-2 text-2xl font-semibold sm:inline-block">
-              Journal
-            </span>
-          </Link>
+            <Link
+              className={cn('flex items-center gap-3', logoFont.className)}
+              href="/"
+            >
+              <Image src="/journal.svg" width={25} height={60} alt="Logo" />
+              <span className="hidden pt-2 text-2xl font-semibold sm:inline-block">
+                Journal
+              </span>
+            </Link>
+          </div>
+          {showMenu && (
+            <button
+              onClick={() => setIsOpen(true)}
+              className="mx-5 block w-full grow lg:hidden"
+            >
+              <LuMenu size={22} />
+            </button>
+          )}
         </div>
-        {showMenu && (
-          <button
-            onClick={() => setIsOpen(true)}
-            className="mx-5 block w-full grow lg:hidden"
-          >
-            <LuMenu size={22} />
-          </button>
-        )}
+        <div className="mx-5 flex items-center space-x-10 sm:space-x-5">
+          {!isSignedIn && <SignInButton />}
+          {isSignedIn && (
+            <>
+              <Link href={`/user/${user.id}`}>
+                <div className="flex items-center">
+                  <LuUserCircle2 size={22} className="sm:hidden" />
+                  <span className="hidden sm:inline-block">
+                    Hi {displayName}!
+                  </span>
+                </div>
+              </Link>
+              <Link href="/feed">
+                <div className="flex items-center">
+                  <LuNewspaper size={22} className="sm:hidden" />
+                  <span className="hidden sm:inline-block">Feed</span>
+                </div>
+              </Link>
+              <Link href="/explore">
+                <div className="flex items-center">
+                  <LuCompass size={22} className="sm:hidden" />
+                  <span className="hidden sm:inline-block">Explore</span>
+                </div>
+              </Link>
+              <Link href="/settings">
+                <div className="flex items-center">
+                  <LuSettings size={22} className="sm:hidden" />
+                  <span className="hidden sm:inline-block">Settings</span>
+                </div>
+              </Link>
+              <SignOutButton>
+                <div className="flex items-center">
+                  <LuLogOut size={22} className="sm:hidden" />
+                  <span className="hidden sm:inline-block">Sign Out</span>
+                </div>
+              </SignOutButton>
+            </>
+          )}
+        </div>
       </div>
-      <div className="mx-5 flex items-center space-x-10 sm:space-x-5">
-        {!isSignedIn && <SignInButton />}
-        {isSignedIn && (
-          <>
-            <Link href={`/user/${user.id}`}>
-              <div className="flex items-center">
-                <LuUserCircle2 size={22} className="sm:hidden" />
-                <span className="hidden sm:inline-block">
-                  Hi {displayName}!
-                </span>
-              </div>
-            </Link>
-            <Link href="/feed">
-              <div className="flex items-center">
-                <LuNewspaper size={22} className="sm:hidden" />
-                <span className="hidden sm:inline-block">Feed</span>
-              </div>
-            </Link>
-            <Link href="/explore">
-              <div className="flex items-center">
-                <LuCompass size={22} className="sm:hidden" />
-                <span className="hidden sm:inline-block">Explore</span>
-              </div>
-            </Link>
-            <Link href="/settings">
-              <div className="flex items-center">
-                <LuSettings size={22} className="sm:hidden" />
-                <span className="hidden sm:inline-block">Settings</span>
-              </div>
-            </Link>
-            <SignOutButton>
-              <div className="flex items-center">
-                <LuLogOut size={22} className="sm:hidden" />
-                <span className="hidden sm:inline-block">Sign Out</span>
-              </div>
-            </SignOutButton>
-          </>
-        )}
-      </div>
-    </div>
+      <div className="mt-[var(--navbar-height)] lg:hidden"></div>
+    </>
   );
 }
